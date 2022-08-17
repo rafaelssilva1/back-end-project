d830ef73ea5d1ef78eda6feec2338f0447a092c1
unknown <rafaelssilva1994@gmail.com>
2022-08-17 09:16:11 +0100

    code cleanup

--- views/admin.php
+++ views/admin.php
@@ -10,9 +10,7 @@
 </head>
 <body>
     <?php include("views/header.php"); ?>
-
-    <?php var_dump($data); ?>
-
+    
     <div class="container padding-bottom">
         <div class="movieslideshow__header">
             <h2>Create Movie</h2>
